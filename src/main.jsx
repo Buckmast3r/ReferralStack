9bf36911c720a69d2e4fd68a633687b954bfb41d
pi-swap-test <recording.buck@gmail.com>
2025-04-26 23:57:24 -0600

    added index.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { AuthProvider } from '@/context/AuthContext'; // Use alias '@'
-import { ReferralProvider } from '@/context/ReferralContext'; // Use alias '@'
-import App from '@/App'; // Use alias '@'
+import './index.css';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-      <ReferralProvider>
-        <App />
-      </ReferralProvider>
-    </AuthProvider>
+    <App />
   </React.StrictMode>
 );
