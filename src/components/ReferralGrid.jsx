fbb1517a2c5cf9c2cf2081255504aa578b8bdc53
pi-swap-test <recording.buck@gmail.com>
2025-05-10 18:17:26 -0600

    moved edit/delete outside of ReferralCard added debug log onClick handler

--- src/components/ReferralGrid.jsx
+++ src/components/ReferralGrid.jsx
@@ -99,10 +99,13 @@ export default function ReferralGrid() {
           referrals.map((referral) => (
             <div key={referral.id} className="flex flex-col gap-2">
               <ReferralCard referral={referral} />
-              <div className="flex gap-2">
+              <div className="flex gap-2 mt-2">
                 <button
                   className="px-3 py-1 text-sm bg-blue-600 text-white rounded hover:bg-blue-700"
-                  onClick={() => setEditReferral(referral)}
+                  onClick={() => {
+                    console.log('Edit clicked', referral);
+                    setEditReferral(referral);
+                  }}
                 >
                   Edit
                 </button>
